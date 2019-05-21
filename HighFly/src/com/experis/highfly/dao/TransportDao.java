488fdb4f9db35fe78fe92e3520660f373402a9b2
Training <Training@MARANELLO-22-P>
2019-05-21 12:41:20 +0200

    Aggiunto il metodo findAvailableTransport a TransportDaoImpl

--- HighFly/src/com/experis/highfly/dao/TransportDao.java
+++ HighFly/src/com/experis/highfly/dao/TransportDao.java
@@ -1,5 +1,6 @@
 package com.experis.highfly.dao;
 
+import java.sql.Date;
 import java.util.List;
 
 import com.experis.highfly.entities.Transport;
@@ -7,5 +8,7 @@ import com.experis.highfly.entities.Transport;
 public interface TransportDao extends GenericDao<Transport> {
 
 	public List<Transport> findByTransportType(int transportType);
+
+	public List<Transport> findAvailableTransport(int id, Date dateFrom, Date dateTo);
 
 }
--- HighFly/src/com/experis/highfly/dao/impl/TransportDaoImpl.java
+++ HighFly/src/com/experis/highfly/dao/impl/TransportDaoImpl.java
@@ -1,5 +1,6 @@
 package com.experis.highfly.dao.impl;
 
+import java.sql.Date;
 import java.util.List;
 
 import javax.persistence.Query;
@@ -19,6 +20,21 @@ public class TransportDaoImpl extends GenericDaoImpl<Transport> implements Trans
 
 		Query q = em.createQuery("select t from Transport t where t.transportType = :transportType");
 		q.setParameter("transportType", transportType);
+		retList = q.getResultList();
+
+		return retList;
+
+	}
+
+	public List<Transport> findAvailableTransport(int id, Date dateFrom, Date dateTo) {
+		List<Transport> retList = null;
+
+		Query q = em.createNativeQuery(
+				"select t from Transport t where t.type.id = :id and t.id not in ( select t.id from Booking b, Transport t where b.transport = t.id and b.dateFrom > :dateFrom and b.dateTo < :dateTo)");
+		q.setParameter("id", id);
+		q.setParameter("dateFrom", dateFrom);
+		q.setParameter("dateTo", dateTo);
+
 		retList = q.getResultList();
 
 		return retList;
